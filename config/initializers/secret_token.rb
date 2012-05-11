85d4de9205b735e96e700478f07dbb3851af1d7d
Adam Derewecki <adam@causes.com>
2012-05-10 19:39:06 -0700

    initial commit; 'rails new singlyhackathon'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Singlyhackathon::Application.config.secret_token = '391ab254c994cab5a8a5641f3e7cad344c62d24a8b625825750c1fb7c8eca6c52d80b28a2e82864365f13ed8a515117e2760e906156baab85bb0777fcd5fc114'
