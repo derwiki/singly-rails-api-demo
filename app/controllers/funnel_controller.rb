f442930c0899f8f84581d5be0f1f72f1a283e282
Alex Wallace <alex@andjs.com>
2012-05-10 20:48:56 -0700

    Wireframes for a grid of faces

---
+++ app/controllers/funnel_controller.rb
@@ -0,0 +1,4 @@
+class FunnelController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Singlyhackathon::Application.routes.draw do
   match 'auth/:service' => 'auth#service'
   match 'auth_callback' => 'auth#callback'
   match 'logout'        => 'auth#logout'
+  match 'funnel'        => 'funnel#index'
   root :to              => 'default#home'
 end
